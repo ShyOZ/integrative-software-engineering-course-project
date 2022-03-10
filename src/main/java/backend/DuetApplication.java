b9505bbc265e88fb80b164775ab4b4b68e21fa72
soran <soran@192.168.1.141>
2022-03-10 19:18:53 +0200

    changed package name and created api class that will used as controller

--- src/main/java/backend/DuetApplication.java
+++ src/main/java/backend/DuetApplication.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DuEtApplication {
+public class DuetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DuEtApplication.class, args);
+		SpringApplication.run(DuetApplication.class, args);
+		
 	}
 
 }
