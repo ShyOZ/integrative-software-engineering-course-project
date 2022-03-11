a55f4fc438c33c9697984eb39be26d9e05eb4f35
soran <soran@192.168.1.141>
2022-03-11 15:55:37 +0200

    - created instanceApiController class - added constants for the api url - created InstanceBoundry interface

--- src/main/java/backend/DuetApplication.java
+++ src/main/java/backend/DuetApplication.java
@@ -3,12 +3,16 @@ package backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class DuetApplication {
+	
+	public static final String API_PREFIX = "/iob/";
+	public static final String DOMAIN = "2022b.Yaeli.Bar.Gimelshtei";
 
 	public static void main(String[] args) {
 		SpringApplication.run(DuetApplication.class, args);
-		
 	}
 
 }
---
+++ src/main/java/backend/InstanceBoundary.java
@@ -0,0 +1,5 @@
+package backend;
+
+public interface InstanceBoundary { // TODO need to fill
+
+}
