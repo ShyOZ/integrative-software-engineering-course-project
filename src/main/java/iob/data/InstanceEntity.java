706618f66106188a63a1a19ca2d20727ef07fb84
weam <weam@weam-20st0048us>
2022-04-03 21:53:21 +0300

    Implemented Instance entity

--- src/main/java/iob/data/InstanceEntity.java
+++ src/main/java/iob/data/InstanceEntity.java
@@ -1,14 +1,42 @@
 package iob.data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/*
+ * Activity Table
+ * 
+ * Instance ID | Type         | Attributes
+ * InstaceId   | VARCHAR(255) | Map
+ * */
+
 @Entity
 @Table(name = "INSTANCES_TABLE")
 public class InstanceEntity {
-	//TODO: OVERRIDE BASIC IMPLEMENTATION
 	private String instanceId;
+	private String type;
+	//private Map<String, Object> attributes
+	//private InstanceId instanceId
+
+	public InstanceEntity() {
+
+	}
+	
+	public InstanceEntity(String instanceId, String type) {
+		this.instanceId = instanceId;
+		this.type = type;
+	}
+
+	@Column(name="Type")
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 	@Id
 	public String getinstanceId() {
